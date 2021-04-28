f4eed14f79b01545d8603480e308bc9b3fd20cd9
yeyuhang <yeyuhang@appicplay.com>
2021-04-28 11:11:29 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* 
  * @Author       : Eug
  * @Date         : 2021-03-09 16:42:17
- * @LastEditTime : 2021-03-26 11:32:17
+ * @LastEditTime : 2021-04-28 11:05:28
  * @LastEditors  : Eug
  * @Descripttion : Descripttion
  * @FilePath     : /chat/vite.config.js
@@ -12,12 +12,9 @@ export default {
         '/@/': resolve(__dirname, 'src')
     },
     proxy: {
-        // 如果是 /lsbdb 打头，则访问地址如下
-        // '/': 'http://47.93.229.170:3000/',
-        // 如果是 /lsbdb 打头，则访问地址如下
         '/api': {
           target: 'http://47.93.229.170:3000',
-        //   target: 'http://localhost:3000',
+          // target: 'http://localhost:3000',
           changeOrigin: true,
           rewrite: path => path.replace(/^\/api/, '')
         }
